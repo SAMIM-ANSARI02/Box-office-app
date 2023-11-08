6551bc61f7a5d732284ecb434f219310b56a2837
samim ansari <samimansari02@gmail.com>
2023-11-08 16:03:00 +0530

    api fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
- import React from 'react'
+ import React, { useState } from 'react'
  
  const Home = () => {
+  const[searchstr,setsearchstr]=useState('')
+
+  const HandleSearchInput=(ev)=>{
+    setsearchstr(ev.target.value)
+  }
+
+  const onSearch=async(e)=>{
+    e.preventDefault();
+    const response=await fetch(`https://api.tvmaze.com/search/shows?q=${searchstr}`)
+    const body=await response.json();
+    console.log(body)
+  }
    return (
-     <div>Home</div>
+     <div>
+      <form action="" onSubmit={onSearch}>
+        <input type="text" value={searchstr} onChange={HandleSearchInput}/>
+        <button type='submit'>Search</button>
+      </form> 
+     </div>
    )
  }
  
